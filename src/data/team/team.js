2eb4673233fc9acc6fee8d7c8c4a91accc0f2292
basam07 <mbasamfarooq@gmail.com>
2025-03-14 13:11:24 +0500

    team discription code update

--- src/data/team/team.js
+++ src/data/team/team.js
@@ -2,7 +2,7 @@ import { fetchAllTeamDescriptions } from "../../../server/our-team/our_team";
 const ourTeam = await getOurTeam();
 export const teamTileData = {
   title: "Our Team",
-  desc: ourTeam.description,
+  desc: ourTeam[0].description,
   button_text: "Meet The Team",
 };
 
